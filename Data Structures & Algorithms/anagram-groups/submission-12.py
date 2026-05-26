1a0813965c3d24f6356087e7be184e1efeaf8b1d
Farhan <farhanulhoque09@gmail.com>
2026-05-26 19:09:28 -0400

    Add: anagram-groups - submission-12

---
+++ Data Structures & Algorithms/anagram-groups/submission-12.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        result = defaultdict(list)
+
+        for str in strs:
+            count = [0] * 26
+            for char in str:
+                count[ord(char) - ord('a')] += 1
+            result[tuple(count)].append(str)
+        
+        return list(result.values())
+
+        # TC: O(m * n), where m is the number of strings in strs and n is the length of the longest string
+        # SC: O(m * n), stroing all words across all groups
+
