487e479e04277f3d85243e6de5b46042215647e2
Farhan <farhanulhoque09@gmail.com>
2026-07-01 23:18:21 -0400

    Add: two-integer-sum - submission-18

---
+++ Data Structures & Algorithms/two-integer-sum/submission-18.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        copy = []
+        for i, n in enumerate(nums):
+            copy.append([n, i])
+        
+        copy.sort()
+
+        start, end = 0, len(nums) - 1
+
+        while start < end:
+            curSum = copy[start][0] + copy[end][0]
+            if curSum == target:
+                return [min(copy[start][1], copy[end][1]), max(copy[start][1], copy[end][1])]
+            elif curSum < target:
+                start += 1
+            else:
+                end -= 1
+        
+        # TC: O(nlogn)
+            
